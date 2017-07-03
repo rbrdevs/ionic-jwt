668a10bf44c24b023838efbee25b71bdc1df75a4
Raúl Bojo <bojo.raul@gmail.com>
2017-07-03 21:38:06 +0200

    Slim Api + jwt

---
+++ vagrant/server/slim-api/app.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . "/vendor/autoload.php";
+
+$app = new \Slim\App([
+    "settings" => [
+        "displayErrorDetails" => true,
+        "addContentLengthHeader" => false,
+    ]
+]);
+
+//require __DIR__ . "/config/dependencies.php";
+//require __DIR__ . "/config/handlers.php";
+require __DIR__ . "/config/middleware.php";
+$app->get("/", function ($request, $response, $arguments) {
+    print "Here be dragons";
+});
+require __DIR__ . "/routes/token.php";
+//require __DIR__ . "/routes/todos.php";
+$app->run();
---
+++ vagrant/server/slim-api/config/middleware.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Token;
+use Slim\Middleware\JwtAuthentication;
+
+$container = $app->getContainer();
+
+$container["token"] = function ($container) {
+    return new Token;
+};
+
+$container["JwtAuthentication"] = function ($container) {
+    return new JwtAuthentication([
+        "path" => "/",
+        "passthrough" => ["/token", "/info"],
+        "secret" => '123456789',
+        //"logger" => $container["logger"],
+        //"relaxed" => ["192.168.50.52"],
+        "error" => function ($request, $response, $arguments) {
+            return $response
+                ->withHeader("Content-type", "application/problem+json")
+                ->write(json_encode(array('success' => false, 'message' => 'Error en token')));
+        },
+        "callback" => function ($request, $response, $arguments) use ($container) {
+            //$container["token"]->hydrate($arguments["decoded"]);
+        }
+    ]);
+};
+
+$app->add("JwtAuthentication");
+
